9c40aeef579ea323ee8009f09289be1931b3d7a1
XiNGRZ <chenxingyu92@gmail.com>
2019-11-29 11:46:50 +0800

    sm8150: Update aptX blobs from crosshatch
    Change-Id: I45b800700a05bfa6b6ea3a7fa7c0b883bc377dc0

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -41,7 +41,9 @@ PRODUCT_COPY_FILES += \
     vendor/meizu/sm8150-common/proprietary/lib64/libimsmedia_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libimsmedia_jni.so \
     vendor/meizu/sm8150-common/proprietary/lib64/libmmosal.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmmosal.so \
     vendor/meizu/sm8150-common/proprietary/lib64/librcc.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/librcc.so \
-    vendor/meizu/sm8150-common/proprietary/lib64/vendor.qti.imsrtpservice@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.qti.imsrtpservice@1.0.so
+    vendor/meizu/sm8150-common/proprietary/lib64/vendor.qti.imsrtpservice@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.qti.imsrtpservice@1.0.so \
+    vendor/meizu/sm8150-common/proprietary/product/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptXHD_encoder.so \
+    vendor/meizu/sm8150-common/proprietary/product/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptX_encoder.so
 
 PRODUCT_PACKAGES += \
     QtiTelephonyService \
